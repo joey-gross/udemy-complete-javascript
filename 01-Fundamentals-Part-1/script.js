16d65a26de44ba5f55a3958127cecadb90ad7b87
joey-gross <hello@joeygross.com>
2021-12-28 15:22:34 -0600

    modify script.js

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,5 +1,19 @@
 let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!');
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log('23');
+
+let firstName = "Matilda";
+let first = 'jonas';
+let firstNamePerson
+let first_name_person
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+console.log(myFirstJob);
