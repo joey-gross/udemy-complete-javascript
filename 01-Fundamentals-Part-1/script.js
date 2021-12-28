0e0f6df0ba9861036c7649a18fd82decaaad6c5e
joey-gross <hello@joeygross.com>
2021-12-28 16:01:57 -0600

    modified script.js

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -4,6 +4,7 @@ console.log(40 + 8 + 23 - 10);
 console.log('Jonas');
 console.log('23');
 
+// Variable name conventions
 let firstName = "Matilda";
 let first = 'jonas';
 let firstNamePerson
@@ -17,3 +18,22 @@ console.log(firstName);
 console.log(firstName);
 
 console.log(myFirstJob);
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
