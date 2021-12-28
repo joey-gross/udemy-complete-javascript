b270c6e41ab08708b07930921fb3ea1ebda78f63
joey-gross <hello@joeygross.com>
2021-12-28 16:01:42 -0600

    modified assignments.js

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -6,3 +6,12 @@ console.log(country);
 console.log(continent);
 console.log(population);
 
+let isIsland = true;
+console.log(isIsland);
+let language;
+console.log(typeof language);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof isIsland);
+
+
