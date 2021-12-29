1686e3ae0b7303fcfe97a3f1a960295e2176537f
joey-gross <hello@joeygross.com>
2021-12-29 17:25:00 -0600

    modify script.js

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -37,3 +37,39 @@ console.log(typeof year);
 
 year = 1991;
 console.log(typeof year);
+
+let age = 30;
+age = 31;
+
+
+const birthYear = 1991;
+
+
+// example of math operators. use variable to define year to avoid changing in multiple places
+const now = 2037
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3)
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+
+// example of assignment operator
+let x = 10 = 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1 = 101
+x--; // x = x - 1 = 100
+console.log(x)
+
+// Comparison operators 
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
