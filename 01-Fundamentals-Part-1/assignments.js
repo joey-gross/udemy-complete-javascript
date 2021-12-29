b20388dab78341bd7e3cec12c871e5a22de1b5cd
joey-gross <hello@joeygross.com>
2021-12-29 17:24:42 -0600

    modified assignments.js

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -1,17 +1,34 @@
-let country = "United States";
-let continent = "North America";
-let population = "350,000,000"
+const country = "United States";
+const continent = "North America";
+let populationUnitedStates = "350";
+populationUnitedStates += 1;
+
+let populationFinland = "6,000,000";
+console.log(populationUnitedStates >= populationFinland);
+
+let populationAverageCountry = "33,000,000";
+console.log(populationUnitedStates <= populationAverageCountry)
+
 
 console.log(country);
 console.log(continent);
-console.log(population);
+console.log(populationUnitedStates);
 
-let isIsland = true;
+const isIsland = true;
 console.log(isIsland);
-let language;
+const language = "english";
 console.log(typeof language);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof isIsland);
 
+const description =
+    country +
+    ' is in ' +
+    continent +
+    ', and its ' +
+    populationUnitedStates +
+    ' million people speak ' +
+    language;
 
+console.log(description);
