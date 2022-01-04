5f9bd8201969864701f74f4238dd540e15d353c1
joey-gross <hello@joeygross.com>
2022-01-04 17:57:50 -0600

    modify script.js

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -73,3 +73,12 @@ console.log(ageSarah >= 18);
 const isFullAge = ageSarah >= 18;
 
 console.log(now - 1991 > now - 2018);
+
+//  Operator precedence 
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10  
+console.log(x, y);
+
+// Use parentheses () to make sure the operation is executed first 
+const averageAge = (ageJonas + ageSarah) / 2
+console.log(ageJonas, ageSarah, averageAge);
