7d0342fdefb4561d1c990ad9b65b66d5cc5aff43
joey-gross <hello@joeygross.com>
2022-01-10 21:21:34 -0600

    modify challenges

--- 01-Fundamentals-Part-1/challenges.js
+++ 01-Fundamentals-Part-1/challenges.js
@@ -46,3 +46,54 @@ if (BMIMark > BMIJohn) {
 } else {
     console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
 }
+
+// Coding Challege #3
+
+/*const totalDolphins = (96 + 108 + 89);
+console.log(totalDolphins);
+
+const totalKoalas = (88 + 91 + 110);
+console.log(totalKoalas);
+
+
+const averageDolphins = (totalDolphins / 3);
+const averageKoalas = (totalKoalas / 3);
+console.log(averageDolphins);
+console.log(averageKoalas);
+
+if (averageDolphins > averageKoalas) {
+    console.log('Dolphins are the winner!');
+} else if (averageKoalas > averageDolphins) {
+    console.log('Koalas are the winner!');
+} else if (averageDolphins === averageKoalas) {
+    console.log('It was a draw');
+}
+*/
+
+// Bonus 1
+/*
+const scoreDolphins = (97 + 112 + 101) / 3;
+
+const scoreKoalas = (109 + 95 + 106) / 3;
+
+console.log(scoreDolphins, scoreKoalas);
+
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+    console.log('Dolphins are the winner!');
+} else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+    console.log('Koalas are the winner!');
+} else if (scoreDolphins === scoreKoalas && scoreKoalas >= 100 && scoreDolphins >= 100) {
+    console.log('It was a draw');
+} else {
+    console.log('No one wins the trophy');
+}
+*/
+//Coding Challenge #4
+
+const bill = 275;
+
+const tip = bill >= 50 && bill <= 300 ? (bill * 15 / 100) : (bill * 20 / 100);
+
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
